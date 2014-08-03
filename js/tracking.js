f952aba4b28cb48c6b337a7f84fcbb1e13518cdd
peterdotjs <peshin@ebay.com>
2014-08-03 09:17:47 -0700

    Adding user opt out ability for tracking.

--- js/tracking.js
+++ js/tracking.js
@@ -1,13 +1,26 @@
+var optOut = localStorage.getItem("tracking-opt-out"),
+	deferTracking = false;
+
+if(optOut && optOut === 'true'){
+	deferTracking = true;
+}
+
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-34217520-2']);
-_gaq.push(['_trackPageview']);
+if(!deferTracking) {
+	_gaq.push(['_setAccount', 'UA-34217520-2']);
+	_gaq.push(['_trackPageview']);
+}
 
 function sendTracking(category, label) {
-	_gaq.push(['_trackEvent', category, 'clicked', label || "na"]);
+	if(!deferTracking) {
+		_gaq.push(['_trackEvent', category, 'clicked', label || "na"]);
+	}
 };
 
 (function() {
-  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  ga.src = 'https://ssl.google-analytics.com/ga.js';
-  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  if(!deferTracking) {
+	  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	  ga.src = 'https://ssl.google-analytics.com/ga.js';
+	  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  }
 })();
