05917460e1a4d65d6a35c46d1e6ecc89fcbfa913
peterdotjs <peshin@ebay.com>
2014-08-03 09:36:07 -0700

    Updating GA tracking to universal tracking code.

--- js/tracking.js
+++ js/tracking.js
@@ -5,22 +5,21 @@ if(optOut && optOut === 'true'){
 	deferTracking = true;
 }
 
-var _gaq = _gaq || [];
-if(!deferTracking) {
-	_gaq.push(['_setAccount', 'UA-34217520-2']);
-	_gaq.push(['_trackPageview']);
-}
-
 function sendTracking(category, label) {
-	if(!deferTracking) {
-		_gaq.push(['_trackEvent', category, 'clicked', label || "na"]);
+	if(!deferTracking && ga) {
+		ga('send','event', category, 'clicked', label || "na");
 	}
 };
 
-(function() {
-  if(!deferTracking) {
-	  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	  ga.src = 'https://ssl.google-analytics.com/ga.js';
-	  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  }
-})();
+if(!deferTracking) {
+	// Standard Google Universal Analytics code
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga'); // Note: https protocol here
+
+	ga('create', 'UA-34217520-2', 'auto');
+	ga('set', 'checkProtocolTask', function(){}); // Removes failing protocol check. @see: http://stackoverflow.com/a/22152353/1958200
+	ga('require', 'displayfeatures');
+	ga('send', 'pageview', '/index.html');
+}
